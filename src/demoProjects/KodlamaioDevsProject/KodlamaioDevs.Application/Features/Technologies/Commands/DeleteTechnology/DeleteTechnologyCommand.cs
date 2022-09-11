1f6d47402333e474b87bb987dea068f7b91441d1
Emopusta <emreduman225@hotmail.com>
2022-09-11 20:37:55 +0300

    added delete command

--- src/demoProjects/KodlamaioDevsProject/KodlamaioDevs.Application/Features/Technologies/Commands/DeleteTechnology/DeleteTechnologyCommand.cs
+++ src/demoProjects/KodlamaioDevsProject/KodlamaioDevs.Application/Features/Technologies/Commands/DeleteTechnology/DeleteTechnologyCommand.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using AutoMapper;
+using KodlamaioDevs.Application.Features.Technologies.Dtos;
+using KodlamaioDevs.Application.Features.Technologies.Rules;
+using KodlamaioDevs.Application.Services.Repositories;
+using KodlamaioDevs.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,34 @@ using System.Threading.Tasks;
 
 namespace KodlamaioDevs.Application.Features.Technologies.Commands.DeleteTechnology
 {
-    internal class DeleteTechnologyCommand
+    public class DeleteTechnologyCommand : IRequest<DeletedTechnologyDto>
     {
+        public int Id { get; set; }
+
+        public class DeleteTechnologyCommandHandler : IRequestHandler<DeleteTechnologyCommand, DeletedTechnologyDto>
+        {
+            private readonly ITechnologyRepository _technologyRepository;
+            private readonly IMapper _mapper;
+            private readonly TechnologyBusinessRules _technologyBusinessRules;
+
+            public DeleteTechnologyCommandHandler(ITechnologyRepository technologyRepository, IMapper mapper, TechnologyBusinessRules technologyBusinessRules)
+            {
+                _technologyRepository = technologyRepository;
+                _mapper = mapper;
+                _technologyBusinessRules = technologyBusinessRules;
+            }
+
+            public async Task<DeletedTechnologyDto> Handle(DeleteTechnologyCommand request, CancellationToken cancellationToken)
+            {
+                Technology? technologyToDelete = await _technologyRepository.GetAsync(t => t.Id == request.Id);
+
+                await _technologyBusinessRules.TechnologyMustExistWhenRequested(technologyToDelete);
+
+                Technology deletedTechnology = await _technologyRepository.DeleteAsync(technologyToDelete);
+                DeletedTechnologyDto deletedTechnologyDto = _mapper.Map<DeletedTechnologyDto>(deletedTechnology);
+                return deletedTechnologyDto;
+
+            }
+        }
     }
 }
